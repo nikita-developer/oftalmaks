eeb46f30bdd43868f3ee4ae792f9d1abf0d28f12
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-04-09 16:05:07 +0300

    изменил таймер

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,35 @@
 
 $(document).ready(function() {
-    initializeTimer(); // вызываем функцию инициализации таймера
+    function Start() {
+        var h = 0;
+        var m = 12;
+        var s = 0;
 
-    function initializeTimer() {
-    	var endDate = new Date(2020,3,4,21,21); // получаем дату истечения таймера 1.год,2.месяц-1,3.день,4.часы,5.минуты
-    	var currentDate = new Date(); // получаем текущую дату
-    	var seconds = (endDate-currentDate) / 1000; // определяем количество секунд до истечения таймера
-    	if (seconds > 0) { // проверка - истекла ли дата обратного отсчета
-    		var minutes = seconds/60; // определяем количество минут до истечения таймера
-    		var hours = minutes/60; // определяем количество часов до истечения таймера
-    		minutes = (hours - Math.floor(hours)) * 60; // подсчитываем кол-во оставшихся минут в текущем часе
-    		hours = Math.floor(hours); // целое количество часов до истечения таймера
-    		seconds = Math.floor((minutes - Math.floor(minutes)) * 60); // подсчитываем кол-во оставшихся секунд в текущей минуте
-    		minutes = Math.floor(minutes); // округляем до целого кол-во оставшихся минут в текущем часе
-
-    		setTimePage(hours,minutes,seconds); // выставляем начальные значения таймера
-
-    		function secOut() {
-    		  if (seconds == 0) { // если секунду закончились то
-    			  if (minutes == 0) { // если минуты закончились то
-    				  if (hours == 0) { // если часы закончились то
-    					  showMessage(timerId); // выводим сообщение об окончании отсчета
-    				  }
-    				  else {
-    					  hours--; // уменьшаем кол-во часов
-    					  minutes = 59; // обновляем минуты
-    					  seconds = 59; // обновляем секунды
-    				  }
-    			  }
-    			  else {
-    				  minutes--; // уменьшаем кол-во минут
-    				  seconds = 59; // обновляем секунды
-    			  }
-    		  }
-    		  else {
-    			  seconds--; // уменьшаем кол-во секунд
-    		  }
-    		  setTimePage(hours,minutes,seconds); // обновляем значения таймера на странице
-    		}
-    		timerId = setInterval(secOut, 1000) // устанавливаем вызов функции через каждую секунду
-    	}
+        var timerId = setTimeout(function tick() {
+            if (s != 0) {
+                s = s - 1;
+            } else {
+                if (m != 0) {
+                    m = m - 1;
+                    s = 59;
+                } else {
+                    if (h != 0) {
+                        h = h - 1;
+                        m = 59;
+                        s = 59;
+                    } else {
+                        return
+                    }
+                }
+            }
+            $('.form__count-item:first-child span:first-child').text(h);
+            $('.form__count-item:nth-child(2) span:first-child').text(m);
+            $('.form__count-item:last-child span:first-child').text(s);
+            timerId = setTimeout(tick, 1000);
+        }, 1000)
     }
 
-    function setTimePage(h,m,s) { // функция выставления таймера на странице
-    	var element = $('.form__count-time'); // находим элемент
-    	element.html('<div class="form__count-item"><span>'+h+'</span> <span>часов</span></div> : <div class="form__count-item"><span>'+m+'</span> <span>минут</span></div> : <div class="form__count-item"><span>'+s+'</span> <span>секунд</span></div>') // выставляем новые значения таймеру на странице
-    }
-
-    function showMessage(timerId) { // функция, вызываемая по истечению времени
-    	clearInterval(timerId); // останавливаем вызов функции через каждую секунду
-    }
+    Start();
 
     var wow = new WOW(
         {
